5dd9021af3091fb2534a55fe450d5505ebdec09a
Zachary Godfrey <godfrey.zach@gmail.com>
2025-04-03 01:35:55 -0400

    WIP

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'static',
+	outDir: '../dist',
+	compressHTML: false,
+	trailingSlash: 'ignore',
+	server: {
+		port: 8080,
+		open: '/'
+	},
+	devToolbar: {
+		enabled: false
+	}
+});
